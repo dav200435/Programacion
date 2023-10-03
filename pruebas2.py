5e77ea698c4f9773edc36c6d6e1afb58a7739efd
dav200435 <davacas2005@gmail.com>
2023-10-03 20:45:36 +0200

    03//10/2023

--- pruebas2.py
+++ pruebas2.py
@@ -1,1 +1,14 @@
-nombre = ""
+import sys 
+import os
+citas ={
+    "fecha":"",
+    "tarea":"",
+    "prioridad":""
+    }
+while (True):
+    match menu:
+        case "1":
+            cita[""]= input("dame la fecha ")
+            #mandas tarea
+        case "2":
+            #mandas tarea
